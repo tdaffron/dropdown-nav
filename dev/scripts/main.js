1bab2f2587419a883ccfdcabd3112c0943605563
thejamesdempsey <hello@thejamesdempsey.com>
2013-11-14 09:37:54 -0500

    Fixing main js file

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -129,11 +129,8 @@
             // close nav by touching the partial off-screen content
             document.addEventListener('click', function(e)
             {
-<<<<<<< HEAD
                 if (nav_open && !hasParent(e.target, 'flyout')) {
-=======
-                if (nav_open && !hasParent(e.target, 'nav')) {
->>>>>>> 56fb8b39cdea59279dd8d5dcabcd8a6ffdda2139
+
                     e.preventDefault();
                     app.closeNav();
                 }
